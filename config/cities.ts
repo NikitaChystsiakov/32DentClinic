5d6342a9e101312f28fba77ab909507f2cb9d9c2
Nikita Chystsiakov <nikitachystsiakov@icloud.com>
2026-08-28 11:14:33 +0300

    Add info about minsk, zhlobin

--- config/cities.ts
+++ config/cities.ts
@@ -15,12 +15,11 @@ export const cities: City[] = [
   {
     slug: 'minsk',
     name: 'Минск',
-    phone: '+375 (XX) XXX-XX-XX', // TODO: заполнить позже
-    phoneHref: 'tel:+375000000000', // TODO: заполнить позже
-    address: 'г. Минск, ул. [TODO], XX', // TODO: заполнить позже
-    coordinates: { lat: 53.9, lng: 27.5667 }, // TODO: заполнить позже
-    // TODO: заменить на реальное фото клиники в Минске (интерьер, приёмная, фасад)
-    image: '/images/services/implantaciya.png',
+    phone: '+375 (29) 323-33-88', 
+    phoneHref: 'tel:+375293233388', 
+    address: 'г. Минск, Проспект Победителей, 41', 
+    coordinates: { lat: 53.914870, lng: 27.535996 }, 
+    image: '/clinic/minskMain.webp',
     featureTags: ['Хирургический центр', 'All-on-4 / All-on-6', 'Рассрочка 0%', 'Трансфер'],
     seoTitle: 'Стоматология 32Дент Минск — лечение и имплантация',
     seoDescription: '32Дент Минск: терапия, хирургия, ортодонтия и имплантация зубов. Собственная лаборатория, гарантия 2 года.',
@@ -32,8 +31,7 @@ export const cities: City[] = [
     phoneHref: 'tel:+375297444033',
     address: 'г. Рогачёв, ул. Ленина, 60',
     coordinates: { lat: 53.0833, lng: 30.05 },
-    // TODO: заменить на реальное фото клиники в Рогачёве (если hero-clinic.png — это не Рогачёв)
-    image: '/images/hero-clinic.png',
+    image: '/clinic/reception.jpg',
     featureTags: ['Терапия и Ортопедия', 'Рассрочка 0%', 'ул. Ленина, 60'],
     seoTitle: 'Стоматология 32Дент Рогачёв — терапия и имплантация',
     seoDescription: '32Дент Рогачёв: терапия, хирургия, ортодонтия и имплантация зубов. Собственная лаборатория, гарантия 2 года.',
@@ -41,11 +39,10 @@ export const cities: City[] = [
   {
     slug: 'zhlobin',
     name: 'Жлобин',
-    phone: '+375 (XX) XXX-XX-XX', // TODO: заполнить позже
-    phoneHref: 'tel:+375000000000', // TODO: заполнить позже
-    address: 'г. Жлобин, ул. [TODO], XX', // TODO: заполнить позже
-    coordinates: { lat: 52.8928, lng: 30.0228 }, // TODO: заполнить позже
-    // TODO: заменить на реальное фото клиники в Жлобине (интерьер, приёмная, фасад)
+    phone: '+375 (44) 559-59-01', 
+    phoneHref: 'tel:+375445595901', 
+    address: 'г. Жлобин, ул. Петровского, 44', 
+    coordinates: { lat: 52.8928, lng: 30.0228 },
     image: '/images/services/terapiya.png',
     featureTags: ['Терапия и Эстетика', 'Рассрочка 0%'],
     seoTitle: 'Стоматология 32Дент Жлобин — стоматологическая помощь',
